4477f7a584f5c65a6723fb266f0ea7430fd359b5
kailashTuta <kailashTuta@users.noreply.github.com>
2020-10-06 08:03:27 +0530

    Added Routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-var myApp = angular.module('myApp', []);
+// MODULES
+var myApp = angular.module('myApp', ['ngRoute']);
+
+// ROUTES
+myApp.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+        .when('/', {
+            templateUrl: '../pages/home.html',
+            controller: 'homeController'
+        })
+        .when('/about', {
+            templateUrl: '../pages/about.html',
+            controller: 'aboutController'
+        })
+        .when('/library', {
+            templateUrl: '../pages/library.html',
+            controller: 'libraryController'
+        });
+});
+
+// CONTROLLER
+myApp.controller('homeController', ['$scope', function ($scope) {
+
+}]);
+myApp.controller('aboutController', ['$scope', function ($scope) {
+
+}]);
+myApp.controller('libraryController', ['$scope', function ($scope) {
+
+}]);
